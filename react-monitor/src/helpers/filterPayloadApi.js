8c28553744230c3c9d26949d9332c0a5529f2204
gamasoft84 <richardgama@yahoo.com.mx>
2021-02-15 16:40:15 -0600

    config filter by Services

--- react-monitor/src/helpers/filterPayloadApi.js
+++ react-monitor/src/helpers/filterPayloadApi.js
@@ -1,10 +1,26 @@
 
     export const filterPayload = (payload, api) =>{
+
         switch (api.id) {
+          case 6://SubmitCheckinLeadData
+          if(payload.contents){
+            console.log('fiterGender');
+            payload.contents.forEach(element => {
+              if(element.customerType = "GCUCUIN"){
+                element.companyInfo = null;
+                element.customerInfo.gender = fiterGender( element.customerInfo.gender );
+              }else{
+                element.customerInfo = null;
+              }
+            });
+          }
+          break;    
           case 10://RequestPOVINdata
+          console.log('fiterTitle and fiterGender');
             if(payload.customerType = "GCUCUIN"){
               payload.companyInfo = null;
               payload.customerInfo.title = fiterTitle( payload.customerInfo.title );
+              payload.customerInfo.gender = fiterGender( payload.customerInfo.gender );
             }else{
               payload.customerInfo = null;
             }
@@ -33,4 +49,22 @@
               break;
         }
         return title;
+      }
+
+
+      const fiterGender = (gender) =>{
+        switch (gender) {
+          case 'GCOGEMA':
+            gender = 'MALE';
+            break;    
+          case 'GCOGEFE':
+            gender = 'FEMALE';
+            break;    
+          case 'GCOGENA':
+            gender = 'UNKNOWN';
+            break;                
+          default:
+              break;
+        }
+        return gender;
       }
